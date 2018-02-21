dc17447ab37f484cbf074ebc02236ab91cf8de30
EsraaAS <36539529+EsraaAS@users.noreply.github.com>
2018-02-21 22:18:57 +0200

    Add files via upload

---
+++ Exercises/Esraa Ali/Project1_Traffic_Lights/Project1_Traffic_Lights.ino
@@ -0,0 +1,45 @@
+// Pin 9 has red LED
+// Pin 10 has yellow LED
+// Pin 11 has green LED
+
+int redLEDPin = 9;
+int yellowLEDPin = 10;
+int greenLEDPin = 11;
+
+int redOnTime = 1000;
+int redOffTime = 1000;
+
+int yellowOnTime = 1000;
+int yellowOffTime = 1000;
+
+int greenOnTime = 1000;
+int greenOffTime = 1000;
+
+int i;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode (redLEDPin, OUTPUT);
+pinMode (yellowLEDPin, OUTPUT);
+pinMode (greenLEDPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+digitalWrite (redLEDPin, HIGH);
+delay (redOnTime);
+digitalWrite (redLEDPin, LOW);
+delay (redOffTime);
+
+digitalWrite (yellowLEDPin, HIGH);
+delay (yellowOnTime);
+digitalWrite (yellowLEDPin, LOW);
+delay (yellowOffTime);
+
+digitalWrite (greenLEDPin, HIGH);
+delay (greenOnTime);
+digitalWrite (greenLEDPin, LOW);
+delay (greenOffTime);
+
+}
