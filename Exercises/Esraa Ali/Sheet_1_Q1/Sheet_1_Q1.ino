fcf19ee3ac2a27b509c43d9db726a55c84ed504d
EsraaAS <36539529+EsraaAS@users.noreply.github.com>
2018-02-21 22:49:00 +0200

    Add files via upload

---
+++ Exercises/Esraa Ali/Sheet_1_Q1/Sheet_1_Q1.ino
@@ -0,0 +1,13 @@
+int Pin = 9;
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+digitalWrite (Pin, HIGH);
+delay (500);
+digitalWrite (Pin, LOW);
+delay (1500);
+}
