ca057f5a251ecde0e3d408ceafd470078bdb3286
radwa97 <36549743+Radwa-a7med-mostafa@users.noreply.github.com>
2018-07-06 10:54:16 +0200

    Add files via upload

---
+++ Arduino Code Examples/rawdwaazz/sensor.ino
@@ -0,0 +1,16 @@
+int sensorpin=A1;
+int ledpin=8;
+float x,z;
+void setup() {
+pinMode(ledpin,OUTPUT);
+}
+
+void loop() {
+ x=analogRead(sensorpin);
+ z=(100*5*z)/(1023);
+ if (z>30.0)
+ {digitalWrite(ledpin,HIGH);}
+ else
+ {digitalWrite(ledpin,LOW);}
+
+}
