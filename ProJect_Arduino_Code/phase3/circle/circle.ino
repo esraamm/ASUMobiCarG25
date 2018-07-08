57af1cf2630307506ddafe6d08b99f7df6d55059
EsraaAS <36539529+EsraaAS@users.noreply.github.com>
2018-07-08 05:55:19 +0200

    Add files via upload

---
+++ ProJect_Arduino_Code/phase3/circle/circle.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+#define RB 46  //right forward 
+#define RF 48 // right backward
+#define LF 50 //left forward
+#define LB 52 //left backward 
+const int ena = 3;
+const int enb =10;
+
+void forward(){
+  digitalWrite(RF, HIGH);
+  digitalWrite(RB, LOW);
+  digitalWrite(LF, HIGH);
+  digitalWrite(LB, LOW);
+  Serial.println("forword");
+  }
+
+    void setup() {
+  
+        //////MOTORS PINS//// 
+  pinMode(RF , OUTPUT);
+  pinMode(RB , OUTPUT);
+  pinMode(LF , OUTPUT);
+  pinMode(LB , OUTPUT);
+  pinMode(ena,OUTPUT);
+  pinMode(enb,OUTPUT);
+  }
+  
+void loop() {
+  
+  analogWrite(ena,150);
+  analogWrite(enb,100);
+  forward();
+
+ delay(10000);
+}
+
